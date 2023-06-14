b3f2c21d2aa861ebabe2e124268302b4ad0fe301
AvivSarig <avivs15@gmail.com>
2023-06-14 15:23:55 +0300

    replace levenshteinDistance func with library js-levenshtein, rewarp main.js into rankSet()

--- main.js
+++ main.js
@@ -2,15 +2,21 @@ const { fileToData } = require('./getData.js');
 const { rankText } = require('./rank.js');
 const { calcMatrix } = require('./calc.js');
 
-const path = process.argv[2];
-if (!path) {
-    throw 'Please provide path to data file like this:\n `npm run start <path to data file>`';
-}
-const data = fileToData(path);
-if (!data) {
-    throw 'Please provide a path to a valid JSON file';
+function rankSet() {
+    const path = process.argv[2];
+    if (!path) {
+        throw 'Please provide path to data file like this:\n `npm run start <path to data file>`';
+    }
+    const data = fileToData(path);
+    if (!data) {
+        throw 'Please provide a path to a valid JSON file';
+    }
+
+    const resembleMatrix = rankText(data);
+    const weightedRes = calcMatrix(resembleMatrix);
+
+    return weightedRes;
 }
 
-const resembleMatrix = rankText(data);
-const weightedRes = calcMatrix(resembleMatrix);
-console.log(weightedRes);
+res = rankSet();
+console.log(res);
--- rank.js
+++ rank.js
@@ -1,11 +1,13 @@
-const { levenshteinDistance } = require('./compare.js')
+const levenshtein = require('js-levenshtein');
 
 function rankText(data) {
     n = data.length;
     const matrix = Array(n).fill().map(() => Array(n).fill());
+
     for (let [i, element] of data.entries()) {
         for (let j = i + 1; j < data.length; j++) {
-            const rank = levenshteinDistance(element, data[j]);
+            const dist = levenshtein(element, data[j]);
+            const rank = dist / element.length;
             matrix[i][j] = rank;
             matrix[j][i] = rank;
             if (rank === 1) console.log(i, j)
